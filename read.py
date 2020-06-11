aba48182b8274d26f6e9ac2c50fceafdecf77be2
Katrina <katrinayu89@gmail.com>
2020-06-11 14:36:37 +0800

    add filter fuction

--- read.py
+++ read.py
@@ -13,3 +13,10 @@ for d in data:
 	sum_len = sum_len + len(d)
 
 print('留言评价长度', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '笔留言少于100')
+print(new[0])

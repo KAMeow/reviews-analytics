4c3909e67eba70ffe8882fe5005e5317df8e1fae
Katrina <katrinayu89@gmail.com>
2020-06-11 14:28:48 +0800

    calc reviews

--- read.py
+++ read.py
@@ -6,7 +6,10 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:  # %这个符号，求余数，1000的倍数才印出来
 			print(len(data))
-print(len(data))
-print(data[0])
-pring('-----------------------')
-print(data[1])
+print('档案读取完了，总共有', len(data), '笔资料')
+
+sum_len = 0 # sum 是加总的意思
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('留言评价长度', sum_len/len(data))
